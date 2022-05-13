df4c001b455c377c647fa3cb8ac71d57d8e3e649
Daniel <vansdan@web.de>
2022-05-13 12:36:30 +0200

    Update __init__.py

--- cbpi4-ilc-actor/__init__.py
+++ cbpi4-ilc-actor/__init__.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 @parameters([
     Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)", default_value="192.168.1.151"),
-    Property.Text(label="Actor Variable", configurable=True, description="Actor Variable in SPS", default_value="MAISCHEN.RPBI_HZG"),
+    Property.Text(label="Actor Variable", configurable=True, description="Actor Variable in SPS", default_value="CBPI4.ILC_Actor"),
     #Property.Text(label="Actor Variable - Get", configurable=True, description="Actor Variable in SPS", default_value="MAISCHEN.RPBI_HZG"),
     ])
 class ILCActor(CBPiActor):
@@ -30,31 +30,16 @@ class ILCActor(CBPiActor):
         logger.info("ACTOR 1111 %s ON" % self.id)
         self.state = True
         
-            ip_ilc = self.props.get("IP ILC")
-            variable_ilc = self.props.get("Actor Variable")            
-            url = "http://" + ip_ilc + "/cgi-bin/writeVal.exe?" + variable_ilc + "+" + "1"
-            
-            #try:
-            #r = requests.get(url, timeout=2,4)))
-            r = requests.get(url)
-            #print(r.status_code)
-            #r.close()
-            #except requests.ConnectTimeout()
-            #print('Timed Out!)
+            #ip_ilc = self.props.get("IP ILC")
+            #variable_ilc = self.props.get("Actor Variable")            
+            #url = "http://" + ip_ilc + "/cgi-bin/writeVal.exe?" + variable_ilc + "+" + "1"
+
         
         
 
     async def off(self):
         logger.info("ACTOR %s OFF " % self.id)
         self.state = False
-        
-            ip_ilc = self.props.get("IP ILC")
-            variable_ilc = self.props.get("Actor Variable")            
-            url = "http://" + ip_ilc + "/cgi-bin/writeVal.exe?" + variable_ilc + "+" + "0"
-            
-            #try:
-            #r = requests.get(url, timeout=2,4)))
-            r = requests.get(url)
 
     def get_state(self):
         return self.state
