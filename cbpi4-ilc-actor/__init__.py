fed5495b6f2574f8d2177a9c544cd58363c6e250
Daniel <vansdan@web.de>
2022-11-23 17:33:34 +0100

    Update __init__.py

--- cbpi4-ilc-actor/__init__.py
+++ cbpi4-ilc-actor/__init__.py
@@ -13,40 +13,147 @@ logger = logging.getLogger(__name__)
 @parameters([
     Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)", default_value="192.168.1.151"),
     Property.Text(label="Actor Variable", configurable=True, description="Actor Variable in SPS", default_value="CBPI4.ILC_Actor"),
+    Property.Select(label="Continuous Mode", options=['YES','NO'], description="Enable this if the remote url should be refreshed periodically even if our local actor state hasn't changed"),
+    Property.Number(label="Continuous Interval", configurable=True, description="Refresh interval in seconds used in continuous mode")
     #Property.Text(label="Actor Variable - Get", configurable=True, description="Actor Variable in SPS", default_value="MAISCHEN.RPBI_HZG"),
     ])
-class CustomActor(CBPiActor):
+class ILCActor(CBPiActor):
 
-    @action("action", parameters={})
-    async def action(self, **kwargs):
-        print("Action Triggered", kwargs)
-        pass
     
-    def on_start(self):
-        self.state = False
+    #ip_ilc = self.props.get("IP ILC")
+    #variable_ilc = self.props.get("Actor Variable")            
+    #url = "http://" + ip_ilc + "/cgi-bin/writeVal.exe?" + variable_ilc + "+" + "1"
+    
+    
+    
+    @action("Toggle on off once with 5 second pause", parameters={})
+    async def action(self, **kwargs):
+        logger.info("Action triggered %s los" % kwargs)
+        self.on()
+        await asyncio.sleep(5)
+        self.off()
+        logger.info("Action triggered %s ende" % kwargs)
         pass
+
+    def __init__(self, cbpi, id, props):
+        super().__init__(cbpi, id, props)
+        self.state = False
+        self.continuous_task = None
+
+        self.request_session = requests.Session()
+
+        if self.props.get("Check Certificate", "YES") == "YES":
+            self.request_session.verify = True
+        else:
+            self.request_session.verify = False
+
+        if self.props.get("Http Method", "GET") == "GET":
+            self.httpmethod_get = True
+        else:
+            self.httpmethod_get = False
+    
+        if self.props.get("Continuous Mode", "NO") == "YES":
+            self.continuous_mode = True
+        else:
+            self.continuous_mode = False
+            
+        self.url_on = self.props.get("Target URL On")
+        self.url_off = self.props.get("Target URL Off")
+
+        self.payload_on = self.props.get("Request Body On")
+        self.payload_off = self.props.get("Request Body Off")
+
+        self.basic_auth = None
+        if self.props.get("Basic Authentication Username","") != "":
+            self.basic_auth = HTTPBasicAuth(self.props.get("Basic Authentication Username",""),
+                                            self.props.get("Basic Authentication Password",""))
+
+        self.continuous_interval = float(self.props.get("Continuous Interval", 5))
+
+
+        self.request_session.timeout = float(self.props.get("Request Timeout", 5))
+
+        pass
+
+
+    async def set_continuous_state(self):
+        logger.info('Starting continuous state setter background task interval=%s'% self.continuous_interval)
+        while True:
+            start_time = int(time.time())
+            try:
+                await self.start_request(self.state)
+            except Exception as e:
+                logger.error("Unknown exception: %s" % e)
+            
+            wait_time = start_time + self.continous_interval - int(time.time())
+            if wait_time < 0:
+                logger.warn("Continuous interval kann nicht gehalten werden, da zu klein und requests brauchen zu lange")
+            else:
+                await asyncio.sleep(wait_time)
+
+        pass
+
+    async def start_request(self, onoff):
+        if onoff:
+            url = self.url_on
+            payload = self.payload_on
+        else:
+            url = self.url_off
+            payload = self.payload_off
+
+        if payload is not None:
+            payload_logtext=payload.replace('"', '\\"')
+        else:
+            payload_logtext="[not_set]"
+
+        if self.basic_auth is not None:
+            basic_auth_logtext = self.basic_auth.username
+        else:
+            basic_auth_logtext = "[not set]"
+
+        logger.info("HTTPActor type=request_start onoff=%s url=\"%s\" payload=\"%s\" method_getpost=%s user=%s" % (onoff, url, payload_logtext, self.httpmethod_get, basic_auth_logtext))
+        if self.httpmethod_get:
+            response = self.request_session.get(url, data=payload, auth=self.basic_auth)
+        else:
+            response = self.request_session.post(url, data=payload, auth=self.basic_auth)
+
+        logger.info("HTTPActor type=request_done onoff=%s url=\"%s\" http_statuscode=%s response_text=\"%s\"" % (onoff, url, response.status_code, response.text.replace('"', '\\"')))
+
 
     async def on(self, power=0):
-        logger.info("ACTOR 1111 %s ON" % self.id)
+        logger.debug("Actor %s ON" % self.id)
         self.state = True
-        
-            #ip_ilc = self.props.get("IP ILC")
-            #variable_ilc = self.props.get("Actor Variable")            
-            #url = "http://" + ip_ilc + "/cgi-bin/writeVal.exe?" + variable_ilc + "+" + "1"
-
-        
-        
+        await self.start_request(True)
 
     async def off(self):
-        logger.info("ACTOR %s OFF " % self.id)
+        logger.debug("Actor %s OFF" % self.id)
         self.state = False
+        await self.start_request(False)
 
     def get_state(self):
         return self.state
-    
-    async def run(self):
+
+    async def start(self):
         pass
 
+    async def stop(self):
+        if self.continuous_task is not None:
+            self.continuous_task.cancel()
+
+        pass
+
+    async def on_start(self):
+        pass
+
+    async def on_stop(self):
+        pass
+
+    
+    async def run(self):
+        if self.continuous_mode:
+            self.continuous_task = asyncio.create_task(self.set_continuous_state())
+        pass
+        
 def setup(cbpi):
-    cbpi.plugin.register("ILC Actor", CustomActor)
+    cbpi.plugin.register("ILC Actor", ILCActor)
     pass
