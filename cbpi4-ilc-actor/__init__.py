c8b173065582d71d65f1f90e858090ee887082ad
Daniel <vansdan@web.de>
2022-12-02 10:56:01 +0100

    Update __init__.py

--- cbpi4-ilc-actor/__init__.py
+++ cbpi4-ilc-actor/__init__.py
@@ -10,22 +10,21 @@ import time
 
 logger = logging.getLogger(__name__)
 
+
 @parameters([
-    Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)", default_value="192.168.1.151"),
-    Property.Text(label="Actor Variable", configurable=True, description="Actor Variable in SPS", default_value="CBPI4.ILC_Actor"),
-    Property.Text(label="Read Variable", configurable=True, description="Read Variable in SPS", default_value="CBPI4.ILC_Actor"),
-    Property.Select(label="Http Method", options=['GET','POST'], description="HTTP method to use"),
-    Property.Select(label="Check Certificate", options=['YES','NO'], description="Enable or disable TLS certificate checking. This setting has no impact for unencrypted connections"),
-    Property.Number(label="Request Timeout", configurable=True, description="HTTP request timeout in seconds (default 5)", default_value=5),
-    Property.Text(label="Target URL On", configurable=True, description="Target url for state on. Must include a uri scheme (https://...)"),
-    Property.Text(label="Request Body On", configurable=True, description="This request body is passed in the corresponding on request"),
-    Property.Text(label="Target URL Off", configurable=True, description="Target url for state off. Must include a uri scheme (https://...)"),
-    Property.Text(label="Request Body Off", configurable=True, description="This request body is passed in the corresponding off request"),
-    Property.Text(label="Basic Authentication Username", configurable=True, description="Username used for basic authentication"),
-    Property.Text(label="Basic Authentication Password", configurable=True, description="Password used for basic authentication"),
-    Property.Select(label="Continuous Mode", options=['YES','NO'], description="Enable this if the remote url should be refreshed periodically even if our local actor state hasn't changed"),
-    Property.Number(label="Continuous Interval", configurable=True, description="Refresh interval in seconds used in continuous mode")
-    ])
+    Property.Text(label="IP ILC", configurable=True, description="IP Adress of ILC SPS (example: 192.168.1.150)",
+                  default_value="192.168.1.151"),
+    Property.Text(label="Actor Variable", configurable=True, description="Actor Variable in SPS",
+                  default_value="CBPI4.ILC_Actor"),
+    Property.Text(label="Read Variable", configurable=True, description="Read Variable in SPS",
+                  default_value="CBPI4.ILC_Actor"),
+    Property.Number(label="Request Timeout", configurable=True,
+                    description="HTTP request timeout in seconds (default 5)", default_value=5),
+    Property.Select(label="Continuous Mode", options=['YES', 'NO'],
+                    description="Enable this if the remote url should be refreshed periodically even if our local actor state hasn't changed"),
+    Property.Number(label="Continuous Interval", configurable=True,
+                    description="Refresh interval in seconds used in continuous mode")
+])
 class ILCActor(CBPiActor):
 
     @action("Toggle on off once with 5 second pause", parameters={})
@@ -37,29 +36,23 @@ class ILCActor(CBPiActor):
         logger.info("Action triggered %s ende" % kwargs)
         pass
 
+    #Startparameter---------------------------------------------------------------------------------------------------
+
     def __init__(self, cbpi, id, props):
         super().__init__(cbpi, id, props)
         self.state = False
         self.continuous_task = None
 
         self.request_session = requests.Session()
-
-        if self.props.get("Check Certificate", "YES") == "YES":
-            self.request_session.verify = True
-        else:
-            self.request_session.verify = False
-
-        if self.props.get("Http Method", "GET") == "GET":
-            self.httpmethod_get = True
-        else:
-            self.httpmethod_get = False
-    
-        if self.props.get("Continuous Mode", "NO") == "YES":
-            self.continuous_mode = True
-        else:
-            self.continuous_mode = False
-         
-            
+        self.request_session.verify = False
+        
+        #if self.props.get("Continuous Mode", "NO") == "YES":
+        self.continuous_mode = True
+        #else:
+        #    self.continuous_mode = False
+        
+        #http://192.168.1.152/cgi-bin/writeVal.exe?WOHNEN.WOH_LICHT_COUCHV+1
+        
         self.variable_ilc = self.props.get("Actor Variable")
         self.variable_ilc_read = self.props.get("Read Variable")       
         self.ip_ilc = self.props.get("IP ILC")
@@ -69,32 +62,24 @@ class ILCActor(CBPiActor):
         
         #url = "http://" + ip_ilc + "/cgi-bin/readVal.exe?" + variable_ilc
         self.url_read = "http://" + self.ip_ilc + "/cgi-bin/readVal.exe?" + self.variable_ilc_read
-            
-        #self.url_on = self.props.get("Target URL On")
-        #self.url_off = self.props.get("Target URL Off")
 
-        self.payload_on = self.props.get("Request Body On")
-        self.payload_off = self.props.get("Request Body Off")
-
+        self.payload_on = None
+        self.payload_off = None
         self.basic_auth = None
-        if self.props.get("Basic Authentication Username","") != "":
-            self.basic_auth = HTTPBasicAuth(self.props.get("Basic Authentication Username",""),
-                                            self.props.get("Basic Authentication Password",""))
-
         self.continuous_interval = float(self.props.get("Continuous Interval", 5))
-
-
         self.request_session.timeout = float(self.props.get("Request Timeout", 5))
-
+        self.req_type = "read"
+        
         pass
 
+    #Funktion set continous state-----------------------------------------------------------------------------------
 
     async def set_continuous_state(self):
         logger.info('Starting continuous state setter background task interval=%s'% self.continuous_interval)
         while True:
             start_time = int(time.time())
             try:
-                await self.start_request(self.state)
+                await self.start_request(self.req_type)
             except Exception as e:
                 logger.error("Unknown exception: %s" % e)
             
@@ -106,48 +91,49 @@ class ILCActor(CBPiActor):
 
         pass
 
-    async def start_request(self, onoff):
-        if onoff:
+    #Funktion starte Request---------------------------------------------------------------------------------------
+    
+    async def start_request(self, req_type):
+        if req_type == "ein":
             url = self.url_on
             payload = self.payload_on
-        else:
+        if req_type == "aus":
             url = self.url_off
             payload = self.payload_off
+        if req_type == "read":
+            url = self.url_read
+            payload = self.payload_off
+        logger.info("ILCActor type=request_start onoff=%s url=\"%s\"" % (req_type, url))
 
-        if payload is not None:
-            payload_logtext=payload.replace('"', '\\"')
-        else:
-            payload_logtext="[not_set]"
+        response = self.request_session.get(url, data=payload, auth=self.basic_auth)
 
-        if self.basic_auth is not None:
-            basic_auth_logtext = self.basic_auth.username
-        else:
-            basic_auth_logtext = "[not set]"
+        logger.info("ILCActor type=request_done onoff=%s url=\"%s\" http_statuscode=%s response_text=\"%s\"" % (req_type, url, response.status_code, response.text.replace('"', '\\"')))
 
-        logger.info("HTTPActor type=request_start onoff=%s url=\"%s\" payload=\"%s\" method_getpost=%s user=%s" % (onoff, url, payload_logtext, self.httpmethod_get, basic_auth_logtext))
-        if self.httpmethod_get:
-            response = self.request_session.get(url, data=payload, auth=self.basic_auth)
-        else:
-            response = self.request_session.post(url, data=payload, auth=self.basic_auth)
-
-        logger.info("HTTPActor type=request_done onoff=%s url=\"%s\" http_statuscode=%s response_text=\"%s\"" % (onoff, url, response.status_code, response.text.replace('"', '\\"')))
-
+    #Funktion on---------------------------------------------------------------------------------------------------
 
     async def on(self, power=0):
         logger.debug("Actor %s ON" % self.id)
         self.state = True
-        await self.start_request(True)
+        await self.start_request("ein")
+
+    #Funktion off--------------------------------------------------------------------------------------------------
 
     async def off(self):
         logger.debug("Actor %s OFF" % self.id)
         self.state = False
-        await self.start_request(False)
+        await self.start_request("aus")
+
+    #Funktion get_state--------------------------------------------------------------------------------------------
 
     def get_state(self):
         return self.state
 
+    #Funktion start------------------------------------------------------------------------------------------------
+
     async def start(self):
         pass
+
+    #Funktion stop-------------------------------------------------------------------------------------------------
 
     async def stop(self):
         if self.continuous_task is not None:
@@ -155,19 +141,25 @@ class ILCActor(CBPiActor):
 
         pass
 
+    #Funktion on_start---------------------------------------------------------------------------------------------
+
     async def on_start(self):
         pass
+
+    #Funktion on_stop----------------------------------------------------------------------------------------------
 
     async def on_stop(self):
         pass
 
-    
+    #Funktion run--------------------------------------------------------------------------------------------------
+
     async def run(self):
         if self.continuous_mode:
             self.continuous_task = asyncio.create_task(self.set_continuous_state())
         pass
 
-
+#Definitionsende----------------------------------------------------------------------------------------------------
+    
 def setup(cbpi):
     cbpi.plugin.register("ILC Actor", ILCActor)
     pass
